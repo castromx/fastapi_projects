e955223a246d53606a197ac419824fef7bfc129d
castromx <96194271+castromx@users.noreply.github.com>
2024-10-25 19:59:41 +0200

    fix crypt/decrypt pass

--- API auth_item/database/utils.py
+++ API auth_item/database/utils.py
@@ -36,14 +36,14 @@ def decode_jwt(token: str | bytes, public_key: str = settings.auth_jwt.public_ke
     return decoded
 
 
-def hash_password(password: str) -> bytes:
+def hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
     pwd_bytes: bytes = password.encode()
-    return bcrypt.hashpw(pwd_bytes, salt)
+    hashed = bcrypt.hashpw(pwd_bytes, salt)
+    return hashed.decode()
 
 
-def validate_password(password: str, hashed_password: bytes) -> bool:
-    return bcrypt.checkpw(
-        password=password.encode(),
-        hashed_password=hashed_password,
-    )
+
+def validate_password(password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
+
--- API auth_item/migrations/env.py
+++ API auth_item/migrations/env.py
@@ -1,14 +1,15 @@
 from logging.config import fileConfig
+
+from sqlalchemy import engine_from_config
+from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlalchemy import pool
-from sqlalchemy.ext.asyncio import create_async_engine
+
 from alembic import context
 from database.database import Base, DATABASE_URL
-
+from database.models import *
 config = context.config
 
-
 config.set_main_option('sqlalchemy.url', DATABASE_URL)
-
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
@@ -16,6 +17,17 @@ if config.config_file_name is not None:
 target_metadata = Base.metadata
 
 def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -28,9 +40,18 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 async def run_migrations_online() -> None:
-    connectable = create_async_engine(
-        config.get_main_option("sqlalchemy.url"),
-        poolclass=pool.NullPool,
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = AsyncEngine(
+        engine_from_config(
+            config.get_section(config.config_ini_section, {}),
+            prefix="sqlalchemy.",
+            poolclass=pool.NullPool,
+        )
     )
 
     async with connectable.connect() as connection:
@@ -38,8 +59,7 @@ async def run_migrations_online() -> None:
 
 def do_run_migrations(connection):
     context.configure(
-        connection=connection,
-        target_metadata=target_metadata
+        connection=connection, target_metadata=target_metadata
     )
 
     with context.begin_transaction():
