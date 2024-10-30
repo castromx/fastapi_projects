5dea3a8758d98f88cccaf4ed9742d743b47c3171
castromx <96194271+castromx@users.noreply.github.com>
2024-10-30 21:19:47 +0100

    add register/login tests

--- API auth_item/tests/test_main.py
+++ API auth_item/tests/test_main.py
@@ -1,10 +1,35 @@
 import pytest
 from httpx import AsyncClient
-from test_config import ac
+from main import app
+
 
 @pytest.mark.asyncio
-async def test_first_point(ac: AsyncClient):
-    response = await ac.get("/")
+async def test_first_point():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.get("/")
     assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_register_user():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.post(
+            "/register/",
+            json={"username": "testuser",
+                  "password": "testpassword",
+                  "email": "test@example.com"
+                  })
+    assert response.status_code == 307
+
+
+@pytest.mark.asyncio
+async def test_login_user():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.post(
+            "/login/",
+            data={"username": "testuser", 
+                  "password": "testpassword"})
+    assert response.status_code == 307
+
 
 
